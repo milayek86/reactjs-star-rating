bc1753bd3e9335ebd02112a4c5c5abb6eec64340
milayek86 <layekrohan86@gmail.com>
2024-04-02 00:58:26 +0530

    Updates

---
+++ precache-manifest.2c0c3ad54c50138f70d9c7a5378ea307.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4540db549e3135ff884fa80c0857e3e0",
+    "url": "./index.html"
+  },
+  {
+    "revision": "60a218ab29c5b74a379e",
+    "url": "./static/css/main.42cc8b22.chunk.css"
+  },
+  {
+    "revision": "55e5c44729cc97fd95b6",
+    "url": "./static/js/2.5b76f5c1.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.5b76f5c1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "60a218ab29c5b74a379e",
+    "url": "./static/js/main.691583b4.chunk.js"
+  },
+  {
+    "revision": "6fce53c7c7713ebf61712cc2929746fa",
+    "url": "./static/js/main.691583b4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1cc5ef4c8ee722f4e062",
+    "url": "./static/js/runtime-main.bf82932a.js"
+  }
+]);
